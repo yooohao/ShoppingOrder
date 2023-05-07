b006b32a6a67227dab1662b5c4accb6fa81e1a78
ZIHAO LIU <92551109+yooohao@users.noreply.github.com>
2023-05-07 12:33:17 +0800

    Preparation steps

---
+++ src/test/java/com/example/shoppingorder/ShoppingOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
